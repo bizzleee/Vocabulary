0fe19ea7fac445c11d824fd82e156d3c9bf80706
Alejandro Pallares <bizzlee200@gmail.com>
2022-08-27 21:11:34 -0500

    Add the navigation of the app

---
+++ Vocabulary/AddLanguageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddLanguageViewController.swift
+//  Vocabulary
+//
+//  Created by Alejandro Pallares on 27/08/22.
+//
+
+import UIKit
+
+class AddLanguageViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+}
